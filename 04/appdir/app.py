5caaa1ccd2f3b794bfa1fc0ac645d53f4913e3ec
zinin-abs-322 <zinin-abs-322@debian4ik.myguest.virtualbox.org>
2025-11-23 16:56:49 +0700

    work in docker

---
+++ 04/appdir/app.py
@@ -0,0 +1,5 @@
+import os
+
+print("Lesson 4")
+print("Current directory: ", os.getcwd())
+print("Script path: ", os.path.dirname(os.path.abspath(__file__)))
---
+++ 07/app.py
@@ -0,0 +1,5 @@
+import os
+
+username = os.getenv("USERNAME")
+
+print(f"USERNAME = {username}")
